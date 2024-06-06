b92139b2e471f436d7e5bf9274861b0a596f186f
payaltvs <payal.jain@tvsmotor.com>
2024-06-06 15:58:00 +0530

    logout done

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { Layout, Typography, Dropdown, Button, Row, Col, Space } from 'antd';
+import { Layout, Typography, Dropdown, Button, Row, Col, Space, Menu } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 import tvsLogo from '../../assets/TVS_Motor_logo.png'; // Adjust the path accordingly
+import { useAuth } from '../authContext';
 
 const { Header } = Layout;
 const { Title } = Typography;
 
-const items = [
-  {
-    label: 'Logout',
-    key: '1',
-  },
-];
 
-const PageHeader = () => (
+const PageHeader = () => {
+
+  const { logout } = useAuth(); // Access logout function from context
+
+  const handleMenuClick = ({ key }) => {
+    if (key === '1') {
+      logout();
+    }
+  };
+
+  const items = [
+    {
+      label: 'Logout',
+      key: '1',
+    },
+  ];
+
+return(
   <Header style={{ backgroundColor: '#fff' }}>
     <Row justify="space-between" align="middle" style={{ borderBottom: '4px solid', borderImage: 'linear-gradient(to right, #1E3D88, #ff0000) 1'}}>
       <Col>
@@ -24,7 +36,7 @@ const PageHeader = () => (
       <Col>
         <div style={{ display: 'flex', alignItems: 'center' }}>
           <span style={{ marginRight: '10px', fontSize: '16px' }}>John Doe</span>
-          <Dropdown menu = {{items}} trigger={['click']}>
+          <Dropdown overlay={<Menu onClick={handleMenuClick} items={items} />} trigger={['click']}>
               <Space>
               <Button style={{ border: 'none', boxShadow: 'none' }}>
                   <DownOutlined />
@@ -37,7 +49,7 @@ const PageHeader = () => (
     </Row>
   </Header>
 );
-
+}
 export default PageHeader;
 
 

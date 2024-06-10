57a7e3311739d3cfb005618627287965c2583497
payaltvs <payal.jain@tvsmotor.com>
2024-06-10 10:19:08 +0530

    reponsive upload

--- src/components/common/header/header.js
+++ src/components/common/header/header.js
@@ -25,54 +25,55 @@ const PageHeader = () =>{
       key: '1',
     },
   ];
-return(
+
+return (
   <>
-  <div className="header">
-    <Row justify="space-between" align="middle" className="header-row">
-      <Col span={8} flex="auto"> </Col>
-      <Col span={8} flex="auto">
-        <Title level={3} className="header-title">Townwise Data Automation</Title>
-      </Col>
-      <Col span={8} className="header-user" flex="none">
-        <span className="header-user-name">John Doe</span>
-        <Dropdown overlay={<Menu onClick={handleMenuClick} items={items} />}  trigger={['click']}>
-          <Space>
-            <Button className="header-button">
-              <DownOutlined />
-            </Button>
-          </Space>
-        </Dropdown>
-        <img src={tvsLogo} alt="TVS Logo" className="header-logo" />
-      </Col>
-    </Row>
-    <br/><br/>
-   
-  </div>
-  <div className='tabs-container'>
-  <Row >
-    <Tabs defaultActiveKey="1" type="card">
-      <TabPane tab="Upload" key="1">
-        <Uploader />
-      </TabPane>
-      <TabPane tab="Status" key="2">
-        {/* Content for Status tab */}
-      </TabPane>
-      <TabPane tab="Report" key="3">
-        {/* Content for Townwise tab */}
-      </TabPane>
-      <TabPane tab="Johari" key="4">
-        {/* Content for Townwise tab */}
-      </TabPane>
-      <TabPane tab="Administration" key="5">
-        {/* Content for Administration tab */}
-      </TabPane>
-    </Tabs>
-    </Row>
-  
-  </div>
+    <div className="header">
+      <Row justify="space-between" align="middle" className="header-row">
+        <Col xs={24} sm={24} md={5} lg={5} xl={5} className="header-logo-container">
+          <img src={tvsLogo} alt="TVS Logo" className="header-logo" />
+        </Col>
+        <Col xs={24} sm={24} md={14} lg={14} xl={14} className="header-title-container">
+          <Title level={3} className="header-title">Townwise Data Automation</Title>
+        </Col>
+        <Col xs={24} sm={24} md={6} lg={6} xl={6} className="header-user">
+          <p className="header-user-name">John Doe</p>
+          <Dropdown overlay={<Menu onClick={handleMenuClick} items={items} />} trigger={['click']}>
+            <Space>
+              <Button className="header-button">
+                <DownOutlined />
+              </Button>
+            </Space>
+          </Dropdown>
+        </Col>
+      </Row>
+      <br/><br/>
+    </div>
+    <div className='tabs-container'>
+      <Row>
+        <Col span={24}>
+          <Tabs defaultActiveKey="1" type="card" tabPosition="top" className="responsive-tabs">
+            <TabPane tab="Upload" key="1">
+              <Uploader />
+            </TabPane>
+            <TabPane tab="Status" key="2">
+              {/* Content for Status tab */}
+            </TabPane>
+            <TabPane tab="Report" key="3">
+              {/* Content for Townwise tab */}
+            </TabPane>
+            <TabPane tab="Johari" key="4">
+              {/* Content for Townwise tab */}
+            </TabPane>
+            <TabPane tab="Administration" key="5">
+              {/* Content for Administration tab */}
+            </TabPane>
+          </Tabs>
+        </Col>
+      </Row>
+    </div>
   </>
 );
 }
+
 export default PageHeader;
-
-
